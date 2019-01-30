0b79bdc6d2dce173c5bdf73be96b573a15d9669f
Stan Bakalov <sunny.bakalov@gmail.com>
2019-01-29 19:23:07 -0700

    added some functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,28 @@ var connection = mysql.createConnection({
     password: process.env.MY_PASSWORD,
     database: ""
 })
+
+connection.connect(function(err) {
+    if (err) throw err;
+    inventorySearch();
+  });
+
+
+  function inventorySearch() {
+    inquirer
+      .prompt([
+      {
+        name: "product",
+        type: "input",
+        message: "What is the id of the product that you would like to buy?",
+      },
+      {
+          name: "quantity",
+          type: "input",
+          message: "How many units would you like to buy?"
+      }
+    ])
+      .then(function(answer) {
+        console.log(answer);
+      });
+  }
