cb507c05586ebc81296aeb87829eece3fe8fd720
Stan Bakalov <sunny.bakalov@gmail.com>
2019-01-31 17:41:27 -0700

    changed some things

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,31 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("dotenv").config();
 
+console.log(`
+################
+*ITEMS FOR SALE*
+################
+  ID  |NAME                                |PRICE      |QUANTITY
+      |                                    |           |
+  1   | nike air yeezy 2 solar             | $850      | 5
+  2   | lebron 8 south beach               | $725      | 10
+  3   | patagonia coat                     | $400      | 50
+  4   | north face coat                    | $350      | 50
+  5   | supreme backpack                   | $300      | 10
+  6   | lebron james lakers jersey         | $120      | 15
+  7   | derrick rose timberwolves jersey   | $120      | 10
+  8   | donovan mitchell jazz jersey       | $120      | 7
+  9   | apple iphone xs                    | $1,000    | 200
+  10  | samsung galaxy note 9              | $1,000    | 150
+#################################################################  
+`)
+
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: process.env.MY_PASSWORD,
-    database: ""
+    database: "bamazon"
 })
 
 connection.connect(function(err) {
@@ -24,16 +43,16 @@ connection.connect(function(err) {
         type: "list",
         message: "What product would you like to buy?",
         choices: [
-            "nike air yeezy 2 solar",
-            "lebron 8 south beach",
-            "patagonia coat",
-            "north face coat",
-            "supreme backpack",
-            "lebron james lakers jersey",
-            "derrick rose timberwolves jersey",
-            "donovan mitchell jazz jersey",
-            "apple iphone xs",
-            "samsung galaxy note 9"
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"
           ]
       },
       {
